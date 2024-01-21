de2d47ad025529e8252e399d6b7dea2a0ee10719
abdo mostafa <abdomostafa19999@gmail.com>
2024-01-21 19:57:31 +0200

    day 1 update

--- app/src/main/java/com/paginator/MainUiState.kt
+++ app/src/main/java/com/paginator/MainUiState.kt
@@ -1,3 +1,3 @@
 package com.paginator
 
-data class MainUiState(val isLoading:Boolean,val items:List<Number>)
+data class MainUiState(val isLoading: Boolean, val items: List<Number>, val errorMessage:String?=null,val isEndReached: Boolean)
--- app/src/main/java/com/paginator/PaginatorViewModel.kt
+++ app/src/main/java/com/paginator/PaginatorViewModel.kt
@@ -5,23 +5,36 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 
-class PaginatorViewModel(val repository: PagintorRepository = PagintorRepository()) : ViewModel() {
+class PaginatorViewModel(private val repository: PagintorRepository = PagintorRepository()) :
+    ViewModel() {
 
     private val initialKey: Int = 0
 
     val defaultPaginator = DefaultPaginator(
         currentKey = initialKey,
         onRequest = { currentKey ->
-            _mainUiState.emit(MainUiState(isLoading = true))
+            _mainUiState.update { it.copy(isLoading = true) }
             repository.loadItems(currentKey, 20)
         },
-        
+        onSuccess = { newItems ->
+            if (newItems.isEmpty())
+                _mainUiState.update { it.copy(isEndReached = true) }
 
-        )
+            _mainUiState.update { it.copy(items = it.items + newItems, isLoading = false) }
+        },
+        onError = { throwable ->
+            _mainUiState.update { it.copy(errorMessage = throwable.message) }
+        },
+        getNextKey = { currentKey ->
+            currentKey + 1
+        }
+    )
 
-    private val _mainUiState = MutableStateFlow(MainUiState(isLoading = false, items = emptyList()))
+    private val _mainUiState =
+        MutableStateFlow(MainUiState(isLoading = false, items = emptyList(), isEndReached = false))
 
     val mainUiState = _mainUiState.asStateFlow()
 }
